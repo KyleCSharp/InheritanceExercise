3f7b15f55557180681d833024ae65dd6c326ffaa
KyleCSharp <kylelrigler@gmail.com>
2022-09-18 14:39:20 -0400

    first commit

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public int NumberOfLegs { get; set; }
+        public int NumberOfEyes { get; set; }
+        public bool HasTail { get; set; }
+        public bool EatsFood { get; set; }
+
+
+
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,34 +6,57 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
+                            // TODO Be sure to follow best practice when creating your classes 
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
+                                         // Create a class Animal -done
+                                         // give this class 4 members that all Animals have in common -done
 
 
+                                         // Create a class Bird -done
+                                         // give this class 4 members that are specific to Bird -done
+                                         // Set this class to inherit from your Animal Class -done
+
+                                         // Create a class Reptile- done
+                                         // give this class 4 members that are specific to Reptile -done 
+                                         // Set this class to inherit from your Animal Class -done
 
 
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
 
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
-             */
+
+                 /*Create an object of your Bird class
+                  give values to your members using the object of your Bird class
+                    *                                                                          -done
+                    * Creatively display the class member values 
+                     */
+            var Bird = new Bird();
+            
+            Bird.HasFeathers=true;
+            Bird.HasBeak=true;       //bird class
+            Bird.HasHollowBones=true;
+            Bird.WarmBlooded = "They are warm blooded animals";
+
+            Bird.EatsFood=true;
+            Bird.NumberOfLegs = 2;
+            Bird.NumberOfEyes = 2; //animal class
+            Bird.HasTail=true;  
+
+                                            /*Create an object of your Reptile class
+                                             *  give values to your members using the object of your Reptile class
+                                             *                                                                                      -done
+                                             * Creatively display the class member values 
+                                             */
+            var reptile = new Reptile();
+
+            reptile.CanChangeColor=true;
+            reptile.HasScales=true;       //reptile 
+            reptile.HasShell=true;
+            reptile.ColdBlooded = "They are cold blooded animals";
+
+            reptile.NumberOfLegs = 4;    //thinking turtles and lizards not including snakes
+            reptile.NumberOfEyes=2;
+            reptile.HasTail=true;      //animal class
+            reptile.EatsFood =true;
+
         }
     }
 }
